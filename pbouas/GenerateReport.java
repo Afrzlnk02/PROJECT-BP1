6b67ee4fa5acb8da2aeff760b00500905eeacfd1
Afrzlnk02 <afrizalnurkholik8@gmail.com>
2025-01-14 10:54:17 +0700

    Add files via upload

---
+++ pbouas/GenerateReport.java
@@ -0,0 +1,41 @@
+
+package PBOUAS;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
+class GenerateReport {
+   
+    void generateReport() {
+        try {
+            String reportPath = "C:\\Users\\andri\\Documents\\NetBeansProjects\\PBOUAS\\src\\pbouas\\reportdata.jasper";
+            String url = "jdbc:mysql://localhost:3306/java_user";
+            String user = "root";
+            String password = "";
+
+            // Membuat koneksi ke database
+            Connection conn = DriverManager.getConnection(url, user, password);
+            
+            // Mengisi laporan dengan data dari database
+            HashMap<String, Object> parameters = new HashMap<>();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reportPath, parameters, conn);
+            
+            // Menampilkan laporan menggunakan JasperViewer
+            JasperViewer.viewReport(jasperPrint, false);
+            
+            // Menutup koneksi
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Terjadi kesalahan: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
+
+
+
